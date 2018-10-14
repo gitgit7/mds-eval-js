76384580d9c13a3f4f90dbb7f84ba82989c92a44
gitgit7 <davidlespeedz@hotmail.fr>
2018-10-14 10:57:36 -0700

    maj

--- exercice-9/index-9.js
+++ exercice-9/index-9.js
@@ -1,23 +1,11 @@
 'use strict';
 
 var checkPalindrome = function (str) {
-    var filter = str.toLowerCase().replace(/ /gi, '');
-    var reverseString = filter.split('').reverse().join('');
-
-    console.log("filter: " + filter);
-    console.log("reverseString: " + reverseString);
-
-    if (filter === reverseString) 
-        return true;
-    else
-        return false; 
-
-    console.log(' ----------------------------------------------------- ');
+    var strtmp = str.toLowerCase().replace(/ /gi, '');
+    var reverseString = strtmp.split('').reverse().join('');
+	return(strtmp === reverseString)
 }
 
-checkPalindrome('LA MALADE pedala mal');
-checkPalindrome('Et la marine va venir à Malte');
-checkPalindrome("À l'étape, épate-la ! ");
 
 // prendre en compte les caractères spéciaux et les ponctuations, y compris les accents.
 
