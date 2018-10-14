08fe8d55b4bb2a7f28872121680bd5adcaa4052f
gitgit7 <davidlespeedz@hotmail.fr>
2018-10-14 16:27:35 -0700

    maj

--- exercice-1/index-1.js
+++ exercice-1/index-1.js
@@ -1,36 +1,17 @@
 'use strict';
 
-var checkPalindrome = function (str) {
-    var filter = str.toLowerCase().replace(/ /gi, '');
-    var reverseString = filter.split('').reverse().join('');
-
-    console.log("filter: " + filter);
-    console.log("reverseString: " + reverseString);
-
-    if (filter === reverseString) 
-        return true;
-    else
-        return false; 
-
-    console.log(' ----------------------------------------------------- ');
+var gridGenerator = function (xAxis, yAxis) {
+	var tab = document.createElement('table');
+	document.body.appendChild(tab);
+		
+	for (var x = 0; x < xAxis; x++) {
+		var tr = document.createElement('tr');
+		tab.appendChild(tr);	
+		
+		for (var y = 0; y < yAxis; y++) {
+			var td = document.createElement('td');
+			tr.appendChild(td);
+			td.innerHTML = "X";
+		}
+	}	
 }
-
-checkPalindrome('LA MALADE pedala mal');
-checkPalindrome('Et la marine va venir à Malte');
-checkPalindrome("À l'étape, épate-la ! ");
-
-// prendre en compte les caractères spéciaux et les ponctuations, y compris les accents.
-
-// ascii 
-// a: 97, z: 122, A: 65, Z: 90
-
-// à finir
-
-
-
-
-
-
-
-
-// demon service dedier a la repetition des taches
